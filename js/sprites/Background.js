57d4af8142f9edf6689c2c643a87c617f04941b1
Jean-Sébastien Beaulieu <folkrav@gmail.com>
2016-09-09 14:08:32 -0400

    removed useless console logs

--- js/sprites/Background.js
+++ js/sprites/Background.js
@@ -7,11 +7,9 @@ Background.prototype.tick = function() {
         var scaling = -(ball.y + 1000) / (blockY + 1000);
         var width = this.bgImage.width / scaling;
         var height = this.bgImage.height / scaling;
-
-        console.log("new width : " + width);
-        console.log(this.bgImage.width);
         var translateX = (this.bgImage.width - width) / 2;
         var translateY = (this.bgImage.height - height) / 2;
+        
         ctx.translate(translateX, translateY);
         ctx.drawImage(this.bgImage, 0, 0, width, height);
         ctx.translate(-translateX, -translateY);
